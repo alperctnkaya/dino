6709d115e28cebaaea853857c8f7840733e48ec8
alperctnkaya <alperctnkayaa@gmail.com>
2026-03-29 14:02:11 -0400

    /SEAS/home/g45307115/dino

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -39,8 +39,8 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    if Path("/SEAS/home/g45307115/dino/checkpoint/").is_dir():
+        p = Path(f"/SEAS/home/g45307115/dino/checkpoint/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
