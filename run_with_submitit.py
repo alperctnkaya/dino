0357aa3874293a9ac6d25c5859cf319e335f3a1f
alperctnkaya <alperctnkayaa@gmail.com>
2026-04-01 19:59:16 -0400

    cpus_per_task=8,

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -113,7 +113,7 @@ def main():
     executor.update_parameters(
         mem_gb=40 * num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
-        cpus_per_task=10,
+        cpus_per_task=8,
         nodes=nodes,
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
